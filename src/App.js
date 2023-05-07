6c8c0336c0fc3e30714e9df3a8a2de65d7dbfb15
Ankith G <ankithg@corra.com>
2023-05-07 19:44:38 +0530

    wiped and changed codebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import AppExtend from "./components/App";
 import {
   BrowserRouter as Router,
@@ -9,7 +7,6 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -29,8 +26,6 @@ function App() {
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
         <Routes>
           <Route path="/" element={<AppExtend />} />
         </Routes>
